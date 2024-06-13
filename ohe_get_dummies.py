dbceb700517353959d0b27d4e48492683e2c84e5
Tanya <tatyana.bu@inbox.ru>
2024-06-13 21:03:32 +0300

    Get dummies solution added and tested

---
+++ ohe_get_dummies.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import random
+              
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+print (data)
+print(pd.get_dummies(data, columns=['whoAmI'], prefix=["I_am"], dtype = int))
