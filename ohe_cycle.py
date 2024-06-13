8e12dfc30bf258add982948f179eb19acdfa045a
Tanya <tatyana.bu@inbox.ru>
2024-06-13 21:10:21 +0300

    Minor changes to cycle solution

--- ohe_cycle.py
+++ ohe_cycle.py
@@ -6,7 +6,6 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
-print (data)
 ohe_simulation = []
 for i in range(0, len(data)):
     check = data.iloc[i].to_string(header=False, index=False) 
@@ -16,4 +15,5 @@ for i in range(0, len(data)):
         temp = [0,1]
     ohe_simulation.append(temp)
 df = pd.DataFrame(ohe_simulation, columns = ['human', 'robot'])
+print (data)
 print(df)
