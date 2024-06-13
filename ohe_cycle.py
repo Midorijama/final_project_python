9a8890e6e61327b96866961511333e381b343eba
Tanya <tatyana.bu@inbox.ru>
2024-06-13 21:04:57 +0300

    Cycle solution added and tested

---
+++ ohe_cycle.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import random
+              
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+print (data)
+ohe_simulation = []
+for i in range(0, len(data)):
+    check = data.iloc[i].to_string(header=False, index=False) 
+    if check == 'human':
+        temp = [1,0]
+    else:
+        temp = [0,1]
+    ohe_simulation.append(temp)
+df = pd.DataFrame(ohe_simulation, columns = ['human', 'robot'])
+print(df)
